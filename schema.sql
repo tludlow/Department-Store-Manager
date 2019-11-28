1c2e5ba0a6fda4ca0613c9c5e00102a93620ec2b
tludlow99 <twjludlow@gmail.com>
2019-11-28 22:44:25 +0000

    Done for the day, more schema making tomorrow

--- schema.sql
+++ schema.sql
@@ -20,5 +20,64 @@ INSERT INTO inventory (ProductDesc, ProductPrice, ProductStockAmount) VALUES ('C
 INSERT INTO inventory (ProductDesc, ProductPrice, ProductStockAmount) VALUES ('Big Mac', 8.55, 420);
 
 
+/* ====================[ ORDERS ]=================== */
+
+
+DROP TABLE orders CASCADE CONSTRAINTS;
+DROP SEQUENCE orders_sequence;
+
+CREATE SEQUENCE orders_sequence START WITH 1 INCREMENT BY 1;
+CREATE TABLE orders (
+	OrderID integer DEFAULT orders_sequence.NEXTVAL NOT NULL,
+	OrderType varchar(30) NOT NULL,
+	OrderCompleted integer NOT NULL,
+	OrderPlaced Date NOT NULL,
+	PRIMARY KEY (OrderID),
+	CHECK (OrderType IN ('InStore', 'Collection', 'Delivery')),
+	CHECK (OrderCompleted IN ('0', '1'))
+);
+
+INSERT INTO orders (OrderType, OrderCompleted, OrderPlaced) VALUES ('InStore', '0', '26-NOV-19');
+INSERT INTO orders (OrderType, OrderCompleted, OrderPlaced) VALUES ('Collection', '0', '24-OCT-19');
+INSERT INTO orders (OrderType, OrderCompleted, OrderPlaced) VALUES ('Collection', '1', '03-OCT-19');
+INSERT INTO orders (OrderType, OrderCompleted, OrderPlaced) VALUES ('Delivery', '1', '18-AUG-19');
+
+
+
+/* ====================[ ORDER_PRODUCTS ]=================== */
+
+
+DROP TABLE order_products CASCADE CONSTRAINTS;
+
+CREATE TABLE order_products (
+	OrderID integer,
+	ProductID integer,
+	ProductQuantity integer NOT NULL,
+	PRIMARY KEY (OrderID, ProductID),
+	FOREIGN KEY (ProductID) REFERENCES inventory(ProductID),
+	FOREIGN KEY (OrderID) REFERENCES orders(OrderID) ON DELETE CASCADE,
+	CHECK (ProductQuantity >= 0)
+);
+
+-- CREATE OR REPLACE TRIGGER order_products_check_quantity 
+-- AFTER INSERT OR UPDATE ON ORDERS
+-- FOR EACH ROW
+-- DECLARE
+-- 	item_qty inventory.ProductStockAmount%type;
+-- BEGIN
+--  SELECT ProductQuantity
+--  INTO item_qty
+--  FROM inventory
+--  WHERE inventory.ProductID = :NEW.ProductID;
+
+-- IF (:NEW.ProductID>=item_qty) THEN
+--  RAISE_APPLICATION_ERROR(-20250,'Trying to order more products than are in stock!!');
+-- END IF;
+-- END;
+
+
+INSERT INTO order_products (OrderID, ProductID, ProductQuantity) VALUES (1, 1, 2);
+
+
 
 /* ======================================= */
