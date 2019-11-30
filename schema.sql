31a48b7ed6c8c1204b43e9f5877d194e6597fb08
tludlow99 <twjludlow@gmail.com>
2019-11-30 00:00:24 +0000

    Amazing crazy procedure...

--- schema.sql
+++ schema.sql
@@ -4,14 +4,18 @@ DROP TABLE inventory CASCADE CONSTRAINTS;
 DROP SEQUENCE inventory_sequence;
 
 CREATE SEQUENCE inventory_sequence START WITH 1 INCREMENT BY 1;
+/
+
 CREATE TABLE inventory (
 	ProductID integer DEFAULT inventory_sequence.NEXTVAL NOT NULL,
 	ProductDesc varchar(30),									
 	ProductPrice numeric(8,2) NOT NULL,
 	ProductStockAmount integer NOT NULL,
-	PRIMARY KEY (ProductID),
-	CHECK (ProductStockAmount >= 0) /*You can't have a negative amount of stocks. */
+	PRIMARY KEY (ProductID)
 );
+
+-- Create the constraint that you can't have negative stock
+ALTER TABLE inventory ADD CONSTRAINT positive_stock_check CHECK (ProductStockAmount >= 0);
 
 INSERT INTO inventory (ProductDesc, ProductPrice, ProductStockAmount) VALUES ('iPhone X', 699.99, 100);
 INSERT INTO inventory (ProductDesc, ProductPrice, ProductStockAmount) VALUES ('Macbook Air', 1299.99, 60);
@@ -27,21 +31,24 @@ DROP TABLE orders CASCADE CONSTRAINTS;
 DROP SEQUENCE orders_sequence;
 
 CREATE SEQUENCE orders_sequence START WITH 1 INCREMENT BY 1;
+/
+
 CREATE TABLE orders (
 	OrderID integer DEFAULT orders_sequence.NEXTVAL NOT NULL,
 	OrderType varchar(30) NOT NULL,
 	OrderCompleted integer NOT NULL,
 	OrderPlaced Date NOT NULL,
-	PRIMARY KEY (OrderID),
-	CHECK (OrderType IN ('InStore', 'Collection', 'Delivery')),
-	CHECK (OrderCompleted IN ('0', '1'))
+	PRIMARY KEY (OrderID)
 );
+ALTER TABLE orders ADD CONSTRAINT valid_order_type_check CHECK (OrderType IN ('InStore', 'Collection', 'Delivery'));
+ALTER TABLE orders ADD CONSTRAINT valid_order_completed_Value_check CHECK (OrderCompleted IN ('0', '1'));
 
-INSERT INTO orders (OrderType, OrderCompleted, OrderPlaced) VALUES ('InStore', '0', '26-NOV-19');
-INSERT INTO orders (OrderType, OrderCompleted, OrderPlaced) VALUES ('Collection', '0', '24-OCT-19');
-INSERT INTO orders (OrderType, OrderCompleted, OrderPlaced) VALUES ('Collection', '1', '03-OCT-19');
-INSERT INTO orders (OrderType, OrderCompleted, OrderPlaced) VALUES ('Delivery', '1', '18-AUG-19');
+-- TODO MAKE A TRIGGER THAT DETERMINES WHETHER OR NOT THE ORDER IS COMPLETED OR NOT!
 
+-- INSERT INTO orders (OrderType, OrderCompleted, OrderPlaced) VALUES ('InStore', '0', '26-NOV-19');
+-- INSERT INTO orders (OrderType, OrderCompleted, OrderPlaced) VALUES ('Collection', '0', '24-OCT-19');
+-- INSERT INTO orders (OrderType, OrderCompleted, OrderPlaced) VALUES ('Collection', '1', '03-OCT-19');
+-- INSERT INTO orders (OrderType, OrderCompleted, OrderPlaced) VALUES ('Delivery', '1', '18-AUG-19');
 
 
 /* ====================[ ORDER_PRODUCTS ]=================== */
@@ -55,29 +62,88 @@ CREATE TABLE order_products (
 	ProductQuantity integer NOT NULL,
 	PRIMARY KEY (OrderID, ProductID),
 	FOREIGN KEY (ProductID) REFERENCES inventory(ProductID),
-	FOREIGN KEY (OrderID) REFERENCES orders(OrderID) ON DELETE CASCADE,
-	CHECK (ProductQuantity >= 0)
+	FOREIGN KEY (OrderID) REFERENCES orders(OrderID) ON DELETE CASCADE
 );
 
--- CREATE OR REPLACE TRIGGER order_products_check_quantity 
--- AFTER INSERT OR UPDATE ON ORDERS
+-- You wouldn't want to order a 0 amount, or negative amount of a product, you just wouldnt include it within this table.
+ALTER TABLE order_products ADD CONSTRAINT positive_order_amount_check CHECK (ProductQuantity > 0);
+
+-- Special type to define the items in an order.
+-- Would look like (productid1, amountordered1, productid2, amountordered2, ....)
+CREATE OR REPLACE TYPE order_items AS TABLE OF integer;
+/
+
+-- Procedure to order products, made using a procedure so that we can rollback on error.
+DROP PROCEDURE create_order;
+CREATE OR REPLACE PROCEDURE create_order (order_type IN varchar2, order_completed IN integer, order_placed IN Date, order_lines IN order_items) AS
+	too_short_order_lines EXCEPTION;
+	invalid_order EXCEPTION;
+	last_order_id integer;
+	currently_processed integer;
+BEGIN 
+	-- We create a savepoint here.
+	SAVEPOINT before_order;
+
+	--Create the new order in orders table
+	INSERT INTO orders (OrderType, OrderCompleted, OrderPlaced) VALUES (order_type, order_completed, order_placed);
+
+	-- Get the ID of the order being inserted.
+	SELECT max(OrderID) INTO last_order_id FROM orders;
+
+  	-- Iterate across all of the order_lines and insert them into the order_products table.
+  	-- If an error occurs whilst doing this the whole order gets rolledback nicely (will probably error if not in stock)
+
+	currently_processed := order_lines.FIRST;
+
+	IF order_lines.count / 2 < 1 THEN
+		RAISE too_short_order_lines;
+	ELSE 
+		FOR i in order_lines.FIRST..order_lines.count / 2 LOOP
+			INSERT INTO order_products (OrderID, ProductID, ProductQuantity) VALUES (last_order_id, order_lines(currently_processed), order_lines(currently_processed+1));
+			currently_processed := currently_processed + 2;
+		END LOOP;
+	END IF;
+EXCEPTION
+	WHEN OTHERS THEN
+		ROLLBACK TO before_order;
+		RAISE invalid_order; 
+END;
+/
+
+
+
+-- Trigger to decrememt the amount of stock for a product following it being added to an order.
+-- Don't need to care about decrementing below 0, this can't happen because the procedure above will handle this error.
+-- CREATE OR REPLACE TRIGGER decrement_stick
+-- AFTER INSERT ON order_products
+-- FOR EACH ROW
+-- BEGIN
+-- 	UPDATE inventory
+-- 	SET ProductStockAmount = ProductStockAmount - :NEW.ProductQuantity
+-- 	WHERE inventory.ProductID = :NEW.ProductID
+-- END;
+-- /
+
+-- Trigger to check that the amount being ordered is at most the amount in the inventory  (amount in stock)
+-- CREATE OR REPLACE TRIGGER order_products_check_quantity_in_inventory
+-- AFTER INSERT OR UPDATE ON order_products
 -- FOR EACH ROW
 -- DECLARE
--- 	item_qty inventory.ProductStockAmount%type;
+-- 	in_stock inventory.ProductStockAmount%type;
 -- BEGIN
---  SELECT ProductQuantity
---  INTO item_qty
+--  SELECT ProductStockAmount
+--  INTO in_stock
 --  FROM inventory
 --  WHERE inventory.ProductID = :NEW.ProductID;
 
--- IF (:NEW.ProductID>=item_qty) THEN
+-- IF (:NEW.ProductQuantity>=in_stock) THEN
 --  RAISE_APPLICATION_ERROR(-20250,'Trying to order more products than are in stock!!');
 -- END IF;
 -- END;
+-- /
 
 
-INSERT INTO order_products (OrderID, ProductID, ProductQuantity) VALUES (1, 1, 2);
-
+--INSERT INTO order_products (OrderID, ProductID, ProductQuantity) VALUES (1, 1, 2);
 
 
 /* ======================================= */
